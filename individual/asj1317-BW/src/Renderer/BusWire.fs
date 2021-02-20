db284d6755f5e66945e487afad16e53ed2528768
sofiajegnell <sofia.jegnell@live.se>
2021-02-20 14:27:28 +0000

    Created skeleton for auto route+select operation and type for wire segment.

--- individual/asj1317-BW/src/Renderer/BusWire.fs
+++ individual/asj1317-BW/src/Renderer/BusWire.fs
@@ -12,7 +12,14 @@ open Helpers
 //------------------------------BusWire Types-----------------------------//
 //------------------------------------------------------------------------//
 
+type Orientation =
+     V | H
 
+type WireSegment = {
+    StartPt: XYPos
+    EndPt: XYPos
+    Orien: Orientation
+}
 /// type for buswires
 /// for demo only. The real wires will
 /// connect to Ports - not symbols, where each symbol has
@@ -25,14 +32,24 @@ type Wire = {
     Id: CommonTypes.ConnectionId 
     SrcSymbol: CommonTypes.ComponentId
     TargetSymbol: CommonTypes.ComponentId
+    WireSegments: WireSegment list
+    WireColor: CommonTypes.HighLightColor
+    WireWidth: int
+    Selected: bool
     }
+
+
 
 type Model = {
     Symbol: Symbol.Model
     WX: Wire list
-    Color: CommonTypes.HighLightColor
     }
 
+type BoundingBox = {
+    Pos: XYPos //Top left corner of box
+    W: int
+    H: int
+}
 //----------------------------Message Type-----------------------------------//
 
 /// Messages to update buswire model
@@ -50,10 +67,10 @@ type Msg =
 /// Useful helper functions
 let posOf x y = {X = x; Y = y}
 
-let addjustedPos (orgPos: XYPos) (adjPos: XYPos) : XYPos =
+let addjustedPos (orgPos: XYPos) (adjPos: (float*float)) : XYPos =
     {
-        X = orgPos.X + adjPos.X
-        Y = orgPos.Y + adjPos.Y
+        X = orgPos.X + (fst adjPos)
+        Y = orgPos.Y + (snd adjPos)
     }
 
 let midPt (aPos: XYPos) (bPos: XYPos) : XYPos =
@@ -62,6 +79,48 @@ let midPt (aPos: XYPos) (bPos: XYPos) : XYPos =
         Y = (aPos.X + bPos.Y)/2.
     }
 
+let containsPt (pt: XYPos) (box: BoundingBox): bool =
+    let bottomLeft = addjustedPos pt (-(float box.W)/2., -(float box.H)/2. )
+    let topRight = addjustedPos pt ((float box.W)/2., (float box.H)/2. )
+    
+    match pt.X, pt.Y with
+    | x,_ when (bottomLeft.X > x ||topRight.X < x) -> false
+    | _,y when (bottomLeft.Y > y || topRight.Y < y) -> false
+    | _,_ -> true
+
+let createBoundingBox (w: int) (h: int) ((pt1, pt2) : XYPos* XYPos) : BoundingBox =
+    {
+        Pos = midPt pt1 pt2
+        W = w
+        H = h
+    }
+
+let autoRoute (startPt: XYPos) (endPt: XYPos) : WireSegment list =
+    let midPt = (posOf endPt.X startPt.Y)
+    [
+        {
+            StartPt = startPt
+            EndPt = midPt
+            Orien = V
+            }
+        {
+            StartPt = midPt
+            EndPt = endPt
+            Orien = H
+        }
+    ]
+     
+
+let createWire (srcP:Symbol.Symbol ) (tgtP: Symbol.Symbol): Wire =
+            {
+            Id=CommonTypes.ConnectionId (uuid())
+            SrcSymbol = srcP.Id
+            TargetSymbol = tgtP.Id
+            WireSegments = (autoRoute srcP.Pos tgtP.Pos)
+            Selected = false
+            WireWidth = 2
+            WireColor = CommonTypes.Red
+        }
 
 
 /// look up wire in WireModel
@@ -81,7 +140,7 @@ type WireRenderProps = {
 /// In general one wire will be multiple (right-angled) segments.
     /// 
 
-let wireSeg (vert1: XYPos) (vert2: XYPos) (color: string) (w: string) : ReactElement = 
+let wireSegView (vert1: XYPos) (vert2: XYPos) (color: string) (w: string) : ReactElement = 
     line [
             X1 vert1.X
             Y1 vert1.Y
@@ -96,7 +155,7 @@ let singleWireView =
     FunctionComponent.Of(
         fun (props: WireRenderProps) ->
             ( List.pairwise [props.SrcP; {X = props.TgtP.X; Y = props.SrcP.Y}; props.TgtP]
-              |> List.map (fun (a,b)  -> wireSeg a b props.ColorP props.StrokeWidthP)
+              |> List.map (fun (a,b)  -> wireSegView a b props.ColorP props.StrokeWidthP)
               |> ofList 
             )
         )
@@ -111,8 +170,8 @@ let view (model:Model) (dispatch: Dispatch<Msg>)=
                 WireP = w
                 SrcP = Symbol.symbolPos model.Symbol w.SrcSymbol 
                 TgtP = Symbol. symbolPos model.Symbol w.TargetSymbol 
-                ColorP = model.Color.Text()
-                StrokeWidthP = "2px" }
+                ColorP = w.WireColor.Text()
+                StrokeWidthP = "%d{w.WireWidth}px"}
             singleWireView props)
     let symbols = Symbol.view model.Symbol (fun sMsg -> dispatch (Symbol sMsg))
     g [] [(g [] wires); symbols]
@@ -132,13 +191,10 @@ let init n () =
                 symbols.[r1],symbols.[n-1] // prevents wire target and source being same
             | r1,r2 -> 
                 symbols.[r1],symbols.[r2]
-        {
-            Id=CommonTypes.ConnectionId (uuid())
-            SrcSymbol = s1.Id
-            TargetSymbol = s2.Id
-        }
+        createWire s1 s2
+
     List.map (fun i -> makeRandomWire()) [1..n]
-    |> (fun wires -> {WX=wires;Symbol=symbols; Color=CommonTypes.Red},Cmd.none)
+    |> (fun wires -> {WX=wires;Symbol=symbols},Cmd.none)
 
 let update (msg : Msg) (model : Model): Model*Cmd<Msg> =
     match msg with
@@ -146,7 +202,7 @@ let update (msg : Msg) (model : Model): Model*Cmd<Msg> =
         let sm,sCmd = Symbol.update sMsg model.Symbol
         {model with Symbol=sm}, Cmd.map Symbol sCmd
     | AddWire _ -> failwithf "Not implemented"
-    | SetColor c -> {model with Color = c}, Cmd.none
+    | SetColor c -> failwithf"Not implemented \{model with Color = c\}, Cmd.none"
     | MouseMsg mMsg -> model, Cmd.ofMsg (Symbol (Symbol.MouseMsg mMsg))
 
 //---------------Other interface functions--------------------//
@@ -155,7 +211,12 @@ let update (msg : Msg) (model : Model): Model*Cmd<Msg> =
 /// or None if no such. Where there are two close wires the nearest is taken. Used
 /// to determine which wire (if any) to select on a mouse click
 let wireToSelectOpt (wModel: Model) (pos: XYPos) : CommonTypes.ConnectionId option = 
-    failwith "Not implemented"
+    failwithf "Not implmented"
+
+
+let getSelected (wModel: Model) : Wire list =
+    wModel.WX
+    |> List.filter (fun w -> w.Selected)    
 
 //----------------------interface to Issie-----------------------//
 let extractWire (wModel: Model) (sId:CommonTypes.ComponentId) : CommonTypes.Component= 
