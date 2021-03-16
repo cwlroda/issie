a52e7e8a66b7df8d1781b48b62e1d215d810b176
Ben Stobbs <ben@stobbs.com>
2021-03-16 09:46:03 +0000

    small changes to overlaps and getSymbolsInTargetArea

--- src/Renderer/Symbol.fs
+++ src/Renderer/Symbol.fs
@@ -126,10 +126,8 @@ let getSymbolsInTargetArea (symModel:Model) (bbox:BBox) : ComponentId List =
     symModel
     |> List.filter
         (fun (sym:Symbol) ->
-            (sym.Component.X >= bbox.Pos.X)
-            && (sym.Component.Y >= bbox.Pos.Y)
-            && (sym.Component.X + sym.Component.W <= bbox.Pos.X + bbox.Width)
-            && (sym.Component.Y + sym.Component.H <= bbox.Pos.Y + bbox.Height)
+            let symBBox = pointsToBBox (posOf sym.Component.X sym.Component.Y) (posOf (sym.Component.X+sym.Component.W) (sym.Component.Y+sym.Component.H))
+            overlaps symBBox bbox
         )
     |> List.map
         (fun (sym:Symbol) -> sym.Id)
