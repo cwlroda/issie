25df543c91bb93991b9649d827895a5a1c2a77cd
sofiajegnell <sofia.jegnell@live.se>
2021-03-17 09:23:25 +0000

    Initial solution to issue #16

--- src/Renderer/BusWire.fs
+++ src/Renderer/BusWire.fs
@@ -171,6 +171,12 @@ let makeWireSegment (wire : Wire) (startPos: XYPos) (endPos: XYPos) : WireSegmen
         Direction = direction
     }
 
+let verticalOverlap (wModel:Model) (box1: BBox) (box2:BBox) = 
+    let isAbove (bb1: BBox) (bb2: BBox) = (bb1.Pos.Y + bb1.Height) <= bb2.Pos.Y
+   
+    not ( isAbove box1 box2 || isAbove box2 box1)
+
+
 let autoRoute (wModel: Model) (wire: Wire) : Map<WireSegId, WireSegment> =
     let startPos = Symbol.portPos wModel.Symbol wire.SrcPort
     let endPos = Symbol.portPos wModel.Symbol wire.TargetPort
@@ -178,9 +184,22 @@ let autoRoute (wModel: Model) (wire: Wire) : Map<WireSegId, WireSegment> =
 
     let wireDir = posDiff endPos startPos
 
-    let defSeg pos wAdj hAdj =
+    let srcHost = Symbol.symbolBBox wModel.Symbol (Symbol.getHostId wModel.Symbol wire.SrcPort )
+    let tgtHost = Symbol.symbolBBox wModel.Symbol (Symbol.getHostId wModel.Symbol wire.TargetPort)
+
+    let vAdj = 
+        match verticalOverlap wModel srcHost tgtHost with
+        | true when srcHost.Pos.Y <= tgtHost.Pos.Y -> 
+            tgtHost.Pos.Y + tgtHost.Height+ 20.
+           
+        | true ->  
+            srcHost.Pos.Y + srcHost.Height + 20.
+        | false -> 
+            midPos.Y
+
+    let defSeg pos vPos hPos =
         match wireDir.X, wireDir.Y with
-        | x, _ when x > 0. ->
+        | x, _ when x > 1. ->
             [
                 snapToGrid pos
                 snapToGrid { X = midPos.X; Y = pos.Y }
@@ -188,12 +207,12 @@ let autoRoute (wModel: Model) (wire: Wire) : Map<WireSegId, WireSegment> =
         | _ ->
             [
                 snapToGrid pos
-                snapToGrid { X = pos.X + hAdj; Y = pos.Y }
-                snapToGrid { X = pos.X + hAdj; Y = wAdj }
+                snapToGrid { X = pos.X + hPos; Y = pos.Y }
+                snapToGrid { X = pos.X + hPos; Y = vPos }
             ]
 
     let initialSegs, finalSegs =
-        defSeg startPos (midPos.Y) 20., List.rev (defSeg endPos midPos.Y -20.)
+        defSeg startPos (vAdj) 20., List.rev (defSeg endPos vAdj -20.)
 
     initialSegs @ finalSegs
     |> List.pairwise
